bc97a778541d182e32678672fe4e0a5e48689f79
wilson campusano <wilsoncampusanojorge@gmail.com>
2015-08-31 21:28:41 -0400

    trying to implement springunit

--- src/main/test/unit/PersonControllerTest_The_Spring_Test_way.java
+++ src/main/test/unit/PersonControllerTest_The_Spring_Test_way.java
@@ -1,6 +1,5 @@
 package unit;
 
-import com.fasterxml.jackson.annotation.JacksonInject;
 import com.wilcage.springtest.Person;
 import com.wilcage.springtest.PersonController;
 import com.wilcage.springtest.PersonService;
@@ -10,66 +9,59 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.View;
 
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.mock;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by wicampusano on 8/31/2015.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
 @WebAppConfiguration
+@ComponentScan(basePackages = "com.wilcage")
 public class PersonControllerTest_The_Spring_Test_way {
 
-    @Autowired
-    private WebAppConfiguration ctx;
+    @InjectMocks
+    private PersonController personController;
+
+    @Mock
+    private PersonService personService;
+
+    @Mock
+    private View mockView;
+
 
     private MockMvc mockMvc;
 
     @Before
-    public void withAnAppContext(){
-        mockMvc = MockMvcBuilders.standaloneSetup(ctx).build();
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(personController)
+                .setSingleView(mockView)
+                .build();
     }
 
     @Test
     public void testGetPerson() throws Exception{
-        ResultActions personControllerResults = mockMvc.perform(get("/person/{id}", 1l));
+        when(personService.getPerson(1l)).thenReturn(new Person(1l, "Chuck"));
+        ResultActions resultActions = mockMvc.perform(get("/person/{id}", 1l));
 
-        personControllerResults.andDo(print())
+        resultActions.andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(view().name("personPage"))
+                .andExpect(view().name("personData"))
                 .andExpect(model().attribute("personData",
-                        allOf( hasProperty("Id", is(1l)) , hasProperty("name"), is("Chuck"))));
+                        allOf( )));
     }
 
-
-    @Configuration
-    public static class TestConfiguration{
-        @Bean
-        public PersonController personController(){
-            PersonController controller = new PersonController();
-            return controller;
-        }
-    }
 }
